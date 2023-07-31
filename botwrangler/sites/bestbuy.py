c13e9eb8c914a1c21a7edc42bf50770566289f73
Azurras <cbell7@icloud.com>
2023-07-31 18:04:31 -0500

    Added BestBuy and Twitter classes

---
+++ botwrangler/sites/bestbuy.py
@@ -0,0 +1,10 @@
+class BestBuy:
+    domain = "https://www.bestbuy.com/"
+    login_step1 = "https://www.bestbuy.com/identity/signin?token=tid%3A2f1772cc-2ff6-11ee-b3ff-0a3a0094f93f"
+    username_input_id = "fld-e"
+    password_input_id = "fld-p1"
+
+    def login(self):
+        pass
+
+
