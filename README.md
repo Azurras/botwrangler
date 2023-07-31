7c0db8971218078426e2879aea7fbd237f338726
Azurras <cbell7@icloud.com>
2023-07-31 18:39:26 -0500

    Added twilo examples

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## What is Bot Wrangler?
 
 ## How do I run Bot Wrangler?
+
+## Useful documents
+
+Twill - https://twill-tools.github.io/twill/python-api.html#using-twill-from-python
--- botwrangler/sites/bestbuy.py
+++ botwrangler/sites/bestbuy.py
@@ -1,3 +1,6 @@
+from twill import browser
+from twill.commands import *
+
 class BestBuy:
     domain = "https://www.bestbuy.com/"
     login_step1 = "https://www.bestbuy.com/identity/signin?token=tid%3A2f1772cc-2ff6-11ee-b3ff-0a3a0094f93f"
@@ -7,4 +10,9 @@ class BestBuy:
     def login(self):
         pass
 
+
+browser.go("https://www.python.org/")
+assert 'Python' in browser.html
+find("Documentation")
+browser.showforms()
 
